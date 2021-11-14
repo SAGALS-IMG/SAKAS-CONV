a0d7884380ab1e55fdc24b452358b103431831af
kawagoe-namazu <a.yoneya@gmail.com>
2021-11-14 12:28:17 +0900

    PW Commoned

--- SAKAS_Conv.dpr
+++ SAKAS_Conv.dpr
@@ -3,11 +3,11 @@ program SAKAS_Conv;
 uses
   Vcl.Forms,
   main in 'main.pas' {Form_Main},
-  SelShape in 'SelShape.pas',
-  Unit_LP in 'Unit_LP.pas' {Form_LP},
-  Unit_PW in 'Unit_PW.pas' {Form_PW},
-  Unit_SAKAS in 'Unit_SAKAS.pas' {Form_SAKAS},
-  Unit_ABOUT in 'Unit_ABOUT.pas' {AboutBox};
+  Unit_ABOUT in 'Unit_ABOUT.pas' {AboutBox},
+  SelShape in '..\_20211115_Com\SelShape.pas',
+  Unit_LP in '..\_20211115_Com\Unit_LP.pas' {Form_LP},
+  Unit_PW in '..\_20211115_Com\Unit_PW.pas' {Form_PW},
+  Unit_SAKAS in '..\_20211115_Com\Unit_SAKAS.pas' {Form_SAKAS};
 
 {$R *.res}
 
@@ -16,8 +16,8 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.Title := 'SAKAS Conv';
   Application.CreateForm(TForm_Main, Form_Main);
+  Application.CreateForm(TAboutBox, AboutBox);
   Application.CreateForm(TForm_PW, Form_PW);
   Application.CreateForm(TForm_SAKAS, Form_SAKAS);
-  Application.CreateForm(TAboutBox, AboutBox);
   Application.Run;
 end.
